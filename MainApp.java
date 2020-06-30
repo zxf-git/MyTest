aefeca262dce91dcc6fb27aeb27a595014254821
zhouxiaofei <zhouxiaofei@mapgis.com>
2020-06-30 15:02:20 +0800

    Add File Signed-off-by: zhouxiaofei <zhouxiaofei@mapgis.com>

---
+++ MainApp.java
@@ -0,0 +1,17 @@
+package com.zd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Administrator on 2019/10/24.
+ */
+public class MainApp extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
